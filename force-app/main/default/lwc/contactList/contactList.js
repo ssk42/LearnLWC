1cc42c10b0ae95c1664d60aa910562e0dd45e853
ssk42 <sthreitz@gmail.com>
2020-09-26 13:43:45 -0400

    adding CCI

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -2,6 +2,7 @@ import { LightningElement, wire } from 'lwc';
 import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import { reduceErrors } from 'c/ldsUtils';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
 const COLUMNS = [
     { label: 'First Name', fieldName: FIRSTNAME_FIELD.fieldApiName, type: 'text', editable: true },
@@ -10,8 +11,13 @@ const COLUMNS = [
 ];
 export default class ContactList extends LightningElement {
     columns = COLUMNS;
+    errors;
     @wire(getContacts)
     contacts;
+    get errors() {
+        return (this.contacts.error) ?
+            reduceErrors(this.contacts.error) : [];
+    }
 
     handleSave(event) {
         this.saveDraftValues = event.detail.draftValues;
